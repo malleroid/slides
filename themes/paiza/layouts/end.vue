58276a000e2676885293a8b36c7e7a4303a44a65
malleroid <38337195+malleroid@users.noreply.github.com>
2026-02-18 19:20:21 +0900

    :fire: no resource logo load

--- themes/paiza/layouts/end.vue
+++ themes/paiza/layouts/end.vue
@@ -2,7 +2,6 @@
   <div class="slidev-layout end text-white" :class="$slidev.themeConfigs?.service">
     <div class="my-auto w-full">
       <slot />
-      <img class="end-logo" src="./logo.svg" />
     </div>
   </div>
 </template>
@@ -11,15 +10,5 @@
 .end {
   background-color: var(--slidev-theme-primary);
   color: var(--slidev-theme-anti);
-}
-
-.end-logo {
-  position: absolute;
-  bottom: -10%;
-  right: -5%;
-  width: 50%;
-  margin: 0;
-  padding: 0;
-  opacity: 0.3;
 }
 </style>
