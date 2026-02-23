55000ec20e2b4f7b1d49d2fa1df7632a4b6a6521
malleroid <38337195+malleroid@users.noreply.github.com>
2026-02-23 16:21:48 +0900

    :sparkles: import cobalt theme

---
+++ themes/cobalt/layouts/profile.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ header?: string }>()
+</script>
+
+<template>
+  <div class="slidev-layout frame profile">
+    <div class="frame-content">
+      <div class="profile-inner">
+        <div v-if="header" class="frame-header">{{ header }}</div>
+        <div class="profile-name"><slot name="name" /></div>
+        <div class="profile-body"><slot /></div>
+      </div>
+    </div>
+  </div>
+</template>
