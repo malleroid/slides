41555b77df9cea28a8dee36919afccea10ef105b
malleroid <38337195+malleroid@users.noreply.github.com>
2026-04-06 20:29:59 +0900

    ✨ add cards and compare layouts to emerald-synth theme

---
+++ themes/emerald-synth/layouts/cards.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  cards: Array<{ title: string; description: string }>
+}>()
+</script>
+
+<template>
+  <div class="slidev-layout frame cards-layout">
+    <div class="frame-content cards-content">
+      <slot />
+      <div class="cards-container">
+        <div v-for="(card, i) in cards" :key="i" class="card">
+          <div class="card-title">{{ card.title }}</div>
+          <div class="card-description">{{ card.description }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
